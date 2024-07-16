6bc51da038ee161bb8935ef592fac9438dcc1d9f
Christoph Daum <christoph.daum@coding-pioneers.com>
2024-07-16 09:46:14 +0200

    Initial Release

---
+++ README.md
@@ -0,0 +1,191 @@
+# Coding Pioneers extended XHProf Tool
+
+## Introduction
+
+While XHProf is a great tool to analyze the performance of your site, it comes with some major limitations. The most
+siginificant one is that you do not see the URL, that was called for a specific request. While this is not a problem if
+you excactly know which request you just made, it is a huge problem, if you just hit save on the WordPress block editor
+and 10-20 requests were made in the background.
+
+And if that was not bad enough, you would only see that $wpdb->query was called 200 times and that it took 10 seconds
+in total, but you would not see the actual queries that were made. Same for HTTP requests using `wp_remote_get()`
+
+This is where our extended XHProf tool comes in. It will log all database queries and all HTTP requests made during the
+request. It will also log the URL that was called and the referrer. This will help you to identify the slow requests and
+the queries that are causing the performance issues.
+
+## Note/Requirements
+
+This toolset was developed based upon the `xhprof_prepend.php` file included in ddev. While it will also work without, I
+will assume that you are using ddev in the following documentation. If you are using another environment please
+contribute your documentation to the project.
+
+For monitoring HTTP Requests the toolset uses information gathering and processed by the plugin
+(Query Monitor)[https://de.wordpress.org/plugins/query-monitor/], if the plugin is not active, this information will not
+be available.
+
+The toolset should also work without WordPress, but it will only provide information about start and end time, duration,
+URL and referrer. It will not provide any information about database queries or HTTP requests.
+
+## Installation
+
+The installation expects that you are using a ddev project. If you are not using ddev, you can still use the XHProf
+tool, but you will need to adjust the installation steps accordingly.
+
+1. install your ddev project
+2. copy these files into /.ddev/xhprof/
+
+## Usage
+
+1. run `ddev xhprof on`
+2. If you have any other prepend file(Ninja Firewall for example) active in a .user.ini file, you must ensure this file
+   require this file is included, otherwise you will not see any profiles.
+3. Open the page you want to profile, or hit save within the WordPress block editor.
+4. Open https://`yourproject`.ddev.site/xhprof/ in your browser, to get the default XHProf output.
+5. To access the additional information run `ddev ssh` to connect to your ddev container and run `cd /tmp/xhprof`
+6. Inside this directory you will find our additional log files as explained below
+
+## Configuration
+
+Out of the box you should not need any configuration. There are two options how you can configure the toolset.
+
+### Using .env variables
+
+All settings will by default try to read the value from the .env file. It is recommended to use this method, as you do
+not have to patch this file.
+
+### Using the xhprof_prepend.php file
+
+Between the comment "Customize these values as needed" and "That's all there is to configure", you will find all
+configurable settings.
+
+#### Env: CP_XHPROF_WPDB_MIN_QUERY_TIME / Const: WPDB_MIN_QUERY_TIME
+
+Default value: `0.1` time in seconds
+
+By changing this you can set the threshold in seconds for the database queries that will be logged. If you set this to
+0, all queries will be logged. It is recommended to set this to a value between 0.1 and 0.5, to ensure you only focus on
+the slow queries.
+
+#### Env: CP_XHPROF_LOG_LOCATION / Const: LOG_LOCATION
+
+Default value: `/tmp/xhprof/`
+
+The location where the log files will be stored. This should be a location that is writable by the webserver user. The
+default value is taken from ddev, only change this if you have an alternative configuration.
+
+#### Env: CP_XHPROF_EMPTY_LOGS / Const: EMPTY_LOGS
+
+Default value: `skip`
+
+This setting will determine what will happen if a DB or HTTP log would be empty. `skip` will prevent the log file from
+being created, `log` or currently any other value will create the log file with a message that no queries were found.
+
+#### Env: CP_XHPROF_LIB_PATH / Const: XHPROF_LIB_PATH
+
+Default value: `/var/xhprof/xhprof_lib/utils/xhprof_lib.php`
+
+This is the path to the `xhprof_lib.php file`. It should not be changed unless you have a different setup.
+
+#### Env: CP_XHPROF_RUNS_PATH / Const: XHPROF_RUNS_PATH
+
+Default value: `/var/xhprof/xhprof_lib/utils/xhprof_runs.php`
+
+This is the path to the `xhprof_runs.php file`. It should not be changed unless you have a different setup.
+
+#### Env: CP_XHPROF_HTTP_LOG_SUFFIX / Const: HTTP_LOG_SUFFIX
+
+Default value: `_http.log`
+
+This is the suffix for the HTTP log files. By using the same value for HTTP and DB logs, you will have both logs in the
+same file.
+
+#### Env: CP_XHPROF_DB_LOG_SUFFIX / Const: DB_LOG_SUFFIX
+
+Default value: `_db.log`
+
+This is the suffix for the DB log files. By using the same value for HTTP and DB logs, you will have both logs in the
+same file.
+
+#### Env: CP_XHPROF_CLI_BASE_LINK / Const: CLI_BASE_LINK
+
+Default value: `(empty)`
+
+This is the base link for the XHProf profile URL, it is recommended to configure this, otherwise you will only see
+relative URLs in your xhprof.log file.
+
+#### Env: CP_XHPROF_RELATIVE_PATH / Const: XHPROF_RELATIVE_PATH
+
+Default value: `/xhprof/`
+
+This is the relative path to the XHProf output. It should not be changed unless you have a different setup.
+
+## Additional log files
+
+In addition to the default XHProf output, we have added some additional log files to help you analyze the performance of
+your site.
+[HEXHASH] is a hash of the XHProf profile URL, which I will use below to refer to the different files.
+
+### xhprof.log
+
+This file contains a summary of the access URLs with some basic performance data. It is useful to get an overview of the
+performance of your site.
+
+The given information should basically provide you with most information you need to see which requests you will need to
+monitor in more detail.
+
+Example:
+
+```
+[Start date & time - End time] ∆ [Duration] | [XHProf profile URL]                                         | URI: [Accessed URL]              | [GET/POST/CLI]([Number of vars], [Size in kb]) | Referrer: [Referrer]
+[2024-07-16 06:16:20 - 06:16:20] ∆ 0.12 s | https://bonedo.ddev.site/xhprof/?run=669610344cbf5&source=ddev | URI: /wp/wp-admin/admin-ajax.php | POST(5 vars, 0.1 kb) | Referrer: /wp/wp-admin/users.php
+```
+
+For CLI requests some information will be empty.
+
+### [HEXHASH]_db.log
+
+This file contains all database queries for the specific request, optionally all requests slower than
+a [query threshold](#env-cp_xhprof_wpdb_min_query_time--const-wpdb_min_query_time)
+that you can configure as described above.
+
+It will also contain some self-explanatory information about all queries.
+
+It will only contain queries that used the `wpdb` object. It will NOT contain any other queries accessing the database
+via any other means.
+
+This file might be missing if no queries were made during the request and the configuration is set
+to [skip empty logs](#env-cp_xhprof_wpdb_min_query_time--const-wpdb_min_query_time).
+
+### [HEXHASH]_http.log
+
+This file requires the plugin [Query Monitor](https://de.wordpress.org/plugins/query-monitor/) to be installed and
+active.
+
+It contains all outgoing HTTP requests using WordPress Core HTTP Request API with request parameter for the specific
+request.
+
+It will NOT contain custom CURL requests or other HTTP requests.
+
+This file might be missing if no HTTP requests were made during the request and the configuration is set
+to [skip empty logs](#env-cp_xhprof_wpdb_min_query_time--const-wpdb_min_query_time).
+
+If Query Monitor is not present, these files will never be created.
+
+### [HEXHASH].ddev.xhprof (default)
+
+This file contains raw information parsed by XHProf, if you call `/xhprof/`.
+
+## Credits
+
+This toolset was developed by the Coding Pioneers team. Please feel free to contribute to this project on our [GitHub page](https://github.com/coding-pioneers/xhprof-toolset) or visit our [website](https://coding-pioneers.com) for more information.
+
+The XHProf tool was originally developed by Facebook 
+
+## License
+
+This project is licensed under the Apache License 2.0 - see the [LICENSE](LICENSE) file for details.
+
+Originally developed at Facebook, XHProf was open sourced in Mar, 2009.
+
+XHProf is now maintained by [Phacility](https://github.com/phacility/xhprof).
---
+++ xhprof_prepend.php
@@ -0,0 +1,299 @@
+<?php
+/**
+ * Extended xhprof_prepend.php
+ *
+ * Customized by Coding Pioneers GmbH.
+ *
+ * Original Authors: ddev contributors, Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For further documentation, see README.md
+ */
+
+namespace CodingPioneers\XHProfToolset;
+
+/**
+ * Customize these values as needed, if possible you can use environment variables.
+ *
+ * @see README.md for details.
+ */
+define( 'WPDB_MIN_QUERY_TIME',  getenv( 'CP_XHPROF_WPDB_MIN_QUERY_TIME' ) ?: 0.1 );
+define( 'LOG_LOCATION',         getenv( 'CP_XHPROF_LOG_LOCATION' )        ?: '/tmp/xhprof/' );
+define( 'EMPTY_LOGS',           getenv( 'CP_XHPROF_EMPTY_LOGS' )          ?: 'skip' );
+define( 'XHPROF_LIB_PATH',      getenv( 'CP_XHPROF_LIB_PATH' )            ?: '/var/xhprof/xhprof_lib/utils/xhprof_lib.php' );
+define( 'XHPROF_RUNS_PATH',     getenv( 'CP_XHPROF_RUNS_PATH' )           ?: '/var/xhprof/xhprof_lib/utils/xhprof_runs.php' );
+define( 'HTTP_LOG_SUFFIX',      getenv( 'CP_XHPROF_HTTP_LOG_SUFFIX' )     ?: '_http.log' );
+define( 'DB_LOG_SUFFIX',        getenv( 'CP_XHPROF_DB_LOG_SUFFIX' )       ?: '_db.log' );
+define( 'CLI_BASE_LINK',        getenv( 'CP_XHPROF_CLI_BASE_LINK' )       ?: '' );
+define( 'XHPROF_RELATIVE_PATH', getenv( 'CP_XHPROF_RELATIVE_PATH' )       ?: '/xhprof/' );
+
+/**
+ * That's all there is to configure. The rest of the file should not need to be modified.
+ */
+
+/**
+ * Class XHProf_Toolset
+ *
+ * This class is used to log additional information about the requests.
+ */
+class XHProfToolset {
+	/**
+	 * @var float $start_time The start time of the request.
+	 */
+	private float $start_time;
+
+	/**
+	 * @var float $end_time The end time of the request.
+	 */
+	private float $end_time;
+
+	/**
+	 * @var string $run_id The run id of the xhprof run.
+	 */
+	private string $run_id;
+
+	/**
+	 * @var string $run_id The run id of the xhprof run.
+	 */
+	private const APP_NAMESPACE = 'ddev';
+
+	/**
+	 * CP_XHProf constructor.
+	 */
+	public function __construct() {
+		$this->start_time = microtime( true );
+
+		xhprof_enable( XHPROF_FLAGS_MEMORY );
+		register_shutdown_function( [ $this, 'completion' ] );
+	}
+
+	/**
+	 * Log the request to a file.
+	 *
+	 * @return void
+	 */
+	private function log_request() {
+		if ( php_sapi_name() === 'cli' ) {
+			$request_type = 'CLI';
+			$uri = implode( ' ', $_SERVER['argv'] );
+			$uri_label = 'Command:';
+			$referrer = 'none';
+		} else {
+			$uri = $_SERVER['REQUEST_URI'] ?? 'none';
+			$uri_label = 'URI:';
+			$referrer = $_SERVER['HTTP_REFERER'] ?? 'none';
+			$local_domain =  ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http' ) . '://' . $_SERVER['HTTP_HOST'] . '/';
+			$referrer = str_replace( $local_domain, '/', $referrer );
+			$request_type = $_SERVER['REQUEST_METHOD'] ?? 'none';
+			if ( $request_type === 'POST' ) {
+				$num_post_vars = count( $_POST );
+				// Add the number of bytes in the POST body to the log entry.
+				$post_body_size = strlen( file_get_contents(  'php://input' ) );
+				$request_type .= sprintf( '(%d vars, %.1f kb)', $num_post_vars, $post_body_size / 1024 );
+			}
+		}
+
+		$runtime = $this->end_time - $this->start_time;
+
+		// Construct the log entry.
+		$log_entry = sprintf(
+			"[%s] ∆ %.2f s | %s | %s %s | %s | Referrer: %s\n",
+			date( 'Y-m-d H:i:s', (int) $this->start_time ) . ' - ' . date( 'H:i:s', (int) $this->end_time ),
+			$runtime,
+			$this->get_profiler_url(),
+			$uri_label,
+			$uri,
+			$request_type,
+			$referrer,
+		);
+
+
+		// Write the log entry to the file
+		file_put_contents( LOG_LOCATION . 'xhprof.log', $log_entry, FILE_APPEND );
+	}
+
+	/**
+	 * Log database queries to a file.
+	 *
+	 * @return void
+	 */
+	private function log_wpdb_queries(): void {
+		global $wpdb;
+		if ( ! isset( $wpdb ) ) {
+			return;
+		}
+
+		if ( ! is_countable( $wpdb->queries ) ) {
+			return;
+		}
+		$total_query_time = 0;
+		$queries_skipped = 0;
+		$log_entry = '';
+		foreach ( $wpdb->queries as $query ) {
+			$total_query_time += $query[1];
+			if ( $query[1] < WPDB_MIN_QUERY_TIME ) {
+				$queries_skipped ++;
+				continue;
+			}
+			$log_entry .= sprintf(
+				"[%.4f s] %s\n--------------------------------------------------------------------\n",
+				$query[1],
+				$query[0],
+			);
+		}
+		if ( empty( $log_entry ) ) {
+			$log_entry = 'No queries took longer than ' . WPDB_MIN_QUERY_TIME . " seconds.\n";
+		}
+
+		$log_intro = sprintf(
+			"[%s] xhprof id: %s, %s\nTotal query time: %.4f s\nTotal queries: %d\nQueries skipped: %d (> %s s)\nShown queries %d\n--------------------------------------------------------------------\n",
+			date('Y-m-d H:i:s'),
+			$this->run_id,
+			$this->get_profiler_url(),
+			$total_query_time,
+			count( $wpdb->queries ),
+			$queries_skipped,
+			WPDB_MIN_QUERY_TIME,
+			count( $wpdb->queries ) - $queries_skipped,
+		);
+
+		$this->write_log( DB_LOG_SUFFIX, $log_entry, $log_intro );
+	}
+
+	/**
+	 * Log HTTP requests to a file.
+	 *
+	 * @return void
+	 */
+	private function log_wp_http_requests(): void {
+		if ( ! class_exists(\QM_Collectors::class ) ) {
+			return;
+		}
+		$collector = \QM_Collectors::get( 'http' );
+
+		if ( $collector === null ) {
+			return;
+		}
+		$data = $collector->get_data();
+
+		$http_requests = isset( $data->http ) && is_countable( $data->http ) ? count( $data->http ) : 0;
+
+		$log_intro = sprintf(
+			"[%s] xhprof id: %s, %s\nTotal http requests time: %.4f s\nTotal http requests: %d\n--------------------------------------------------------------------\n",
+			date('Y-m-d H:i:s'),
+			$this->get_profiler_url(),
+			$this->run_id,
+			$data['ltime'],
+			$http_requests,
+		);
+
+		$log_content = '';
+		if ( is_iterable( $data->http ) ) {
+			foreach ( $data->http as $http_request ) {
+				$log_content .= sprintf(
+					"[%.4f s] %s %s %s (%.1f kb ↑, %.1f kb ↓)\nHeaders: %s\nBody: %s\n--------------------------------------------------------------------\n",
+					$http_request['ltime'],
+					$http_request['args']['method'],
+					$http_request['info']['http_code'],
+					$http_request['url'],
+					$http_request['info']['size_upload'] / 1024,
+					$http_request['info']['size_download'] / 1024,
+					! empty( $http_request['args']['headers'] ) ? print_r( $http_request['args']['headers'], true ) : 'none',
+					! empty( $http_request['args']['body'] ) ? print_r( $http_request['args']['body'], true ) : 'none',
+				);
+			}
+		}
+
+		$this->write_log( HTTP_LOG_SUFFIX, $log_content, $log_intro );
+	}
+
+	/**
+	 * Write a log entry to a file.
+	 *
+	 * @param string $file_suffix Suffix for the logfile.
+	 * @param string $content     Content to write to the log file.
+	 * @param string $log_intro   Intro for the log file.
+	 *
+	 * @return void
+	 */
+	private function write_log( string $file_suffix, string $content, string $log_intro = '' ): void {
+		if ( empty( $content ) && EMPTY_LOGS === 'skip' ) {
+			// Only write a log entry if there is content to write.
+			return;
+		}
+
+		file_put_contents( LOG_LOCATION . $this->run_id . $file_suffix, $log_intro . $content, FILE_APPEND );
+	}
+
+	/**
+	 * Get the profiler URL.
+	 *
+	 * @return string
+	 */
+	private function get_profiler_url(): string {
+		if ( php_sapi_name() === 'cli' ) {
+			$base_link = CLI_BASE_LINK;
+		} else {
+			$base_link = ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' ? "https" : "http" ) . "://" . $_SERVER['HTTP_HOST'];
+		}
+
+		return sprintf('%s?run=%s&source=%s', $base_link . XHPROF_RELATIVE_PATH, $this->run_id, self::APP_NAMESPACE );
+	}
+
+	/**
+	 * Complete the xhprof profiling and write the data to the xhprof_html output and latest.
+	 *
+	 * @return void
+	 */
+	public function completion(): void {
+		$xhprof_data = xhprof_disable();
+		$this->end_time = microtime( true );
+
+		require_once XHPROF_LIB_PATH;
+		require_once XHPROF_RUNS_PATH;
+
+		$xhprof_runs = new \XHProfRuns_Default();
+		$this->run_id = $xhprof_runs->save_run( $xhprof_data, self::APP_NAMESPACE );
+
+		if ( function_exists( 'add_action') ) {
+			// We are within WordPress, so we hook into the shutdown action, to log everything that happens on shutdown().
+			add_action( 'shutdown', [ $this, 'wp_shutdown'], 1000 );
+		} else {
+			// We are not within WordPress, so we just fire log request now.
+			$this->log_request();
+		}
+	}
+
+	/**
+	 * Log the request, database queries and http requests.
+	 *
+	 * Called on the WordPress action 'shutdown'.
+	 *
+	 * @return void
+	 */
+	public function wp_shutdown() {
+		$this->log_request();
+		$this->log_wpdb_queries();
+		$this->log_wp_http_requests();
+	}
+}
+
+$uri = "none";
+if ( ! empty( $_SERVER ) && array_key_exists( 'REQUEST_URI', $_SERVER ) ) {
+	$uri = $_SERVER['REQUEST_URI'];
+}
+// Enable xhprof profiling if we're not on a xhprof page
+if ( extension_loaded( 'xhprof' ) && strpos( $uri, '/xhprof' ) !== 0 ) {
+	new XHProfToolset();
+}
